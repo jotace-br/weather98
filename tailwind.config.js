60bf5fc36d0e49f4dca3866d5945bb6c143aaf17
Júlio César <jc10ferreira@gmail.com>
2024-01-24 12:45:37 -0300

    chore: :wrench: Adds basic configuration to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ export default {
       'ms-medium': 'Medium ,sans-serif',
       'ms-bold': 'Bold, sans-serif',
     },
+    colors: {
+      gray: '#BFBFBF',
+      gray2: '#808080',
+      header: 'linear-gradient(270deg, #1085D2 0%, #00007B 100%)',
+      white: '#fff',
+      black: '#000',
+      teal: '#008080',
+    },
+    boxShadow: {
+      normal:
+        '1px 1px 0px 0px #DBDBDB inset, -1px -1px 0px 0px #808080 inset, 1px 1px 0px 0px #FFF inset, -1px -1px 0px 0px #000 inset',
+      hover:
+        'inset -1px 0 gray, inset -1px 1px #fff, inset -1px -1px gray, inset 0 0 0 1px #fff',
+      clicked:
+        '1px 1px 0px 0px #DBDBDB inset, -1px -1px 0px 0px #808080 inset, 1px 1px 0px 0px #FFF inset, -1px -1px 0px 0px #000 inset',
+      input:
+        '1px 1px 0px 0px #808080 inset, -1px -1px 0px 0px #DFDFDF inset, 1px 1px 0px 0px #0A0A0A inset, -1px -1px 0px 0px #FFF inset',
+      taskbar:
+        'inset -1px 0 gray, inset -1px 1px #fff, inset -1px -1px gray, inset 0 0 0 1px #fff',
+    },
   },
   plugins: [],
 };
