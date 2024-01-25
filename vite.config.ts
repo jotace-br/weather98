f0f0a79a44922d557a3ed92ccabfc51b34f9fc55
Júlio César <jc10ferreira@gmail.com>
2024-01-25 19:53:33 -0300

    fix: :bug: Fixes env import

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,36 @@
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      include: ['**/*.tsx', '**/*.ts'],
-    }),
-  ],
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react({
+        include: ['**/*.tsx', '**/*.ts'],
+      }),
+    ],
+    define: {
+      API_KEY: env.VITE_API_KEY,
+      API_URL: env.VITE_API_URL,
     },
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
+    server: {
+      watch: {
+        usePolling: true,
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['js-big-decimal'],
-  },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+    optimizeDeps: {
+      exclude: ['js-big-decimal'],
+    },
+  };
 });
