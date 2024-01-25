ed6eb5d8215c1b6c6360f678867be5a8154b613b
Júlio César <jc10ferreira@gmail.com>
2024-01-25 20:24:37 -0300

    chore: :ambulance: Removes VITE_API_URL from .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,32 @@
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react({
-        include: ['**/*.tsx', '**/*.ts'],
-      }),
-    ],
-    define: {
-      API_KEY: env.VITE_API_KEY,
-      API_URL: env.VITE_API_URL,
+export default defineConfig({
+  plugins: [
+    react({
+      include: ['**/*.tsx', '**/*.ts'],
+    }),
+  ],
+  // define: {
+  //   API_KEY: process.env.VITE_API_KEY,
+  //   API_URL: process.env.VITE_API_URL,
+  // },
+  server: {
+    watch: {
+      usePolling: true,
     },
-    server: {
-      watch: {
-        usePolling: true,
-      },
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
-    optimizeDeps: {
-      exclude: ['js-big-decimal'],
-    },
-  };
+  },
+  optimizeDeps: {
+    exclude: ['js-big-decimal'],
+  },
 });
