b97997e190915771196c909d665f1215ba2483df
Júlio César <jc10ferreira@gmail.com>
2024-01-25 00:54:03 -0300

    style: :lipstick: Changes some styles

--- src/components/WeatherInfo/CurrentWeather.tsx
+++ src/components/WeatherInfo/CurrentWeather.tsx
@@ -17,13 +17,13 @@ const CurrentWeather = ({
   const { unit } = useWeatherSettings();
 
   return (
-    <div className='flex flex-wrap flex-col items-center w-[50%]'>
+    <div className='w-full flex flex-wrap flex-col items-center sm:w-[50%]'>
       <img
         src={`https://openweathermap.org/img/w/${icon}.png`}
         alt='weather icon'
         width='50px'
         height='50px'
-        className='object-cover w-[50px] h-[50px] sm:w-[75px] sm:h-[75px]'
+        className='object-cover w-[50px] h-[50px] sm:w-[75px] sm:h-[75px] saturate-[5]'
       />
 
       <p className='flex select-all'>
@@ -37,17 +37,18 @@ const CurrentWeather = ({
           {transformUnitToChar(unit)}
         </span>
       </p>
-      <p className='text-[1rem] flex select-all'>
+      <div className='w-full flex justify-center text-[1rem] text-textColor select-all'>
         <span>
           Feels like{' '}
           {isNaN(Number(feelsLike))
             ? 'N/A'
             : String(Math.round(Number(feelsLike)))}
         </span>
+
         <span className='text-xs'>o</span>
-        <span className='flex items-end'>{transformUnitToChar(unit)}</span>
-      </p>
-      <p className='text-[1rem]'>{description}</p>
+        {transformUnitToChar(unit)}
+      </div>
+      <p className='text-[1rem] text-textColor'>{description}</p>
     </div>
   );
 };
