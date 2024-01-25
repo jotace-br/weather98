778b2832fe6f77f5b2479eef1fbee6d5887d3370
Júlio César <jc10ferreira@gmail.com>
2024-01-25 00:06:55 -0300

    chore: :building_construction: Changes the main and app.tsx to display the new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import { useState } from 'react';
-import SearchBar from '~/components/SearchBar';
-import WeatherDisplay from '~/components/WeatherDisplay';
-import { Search } from '~/types/Search';
+import { useEffect, useState } from 'react';
+import { fetchRandomWallpaper } from './api/api';
+import Prompt from './components/Prompt/Prompt';
+import Taskbar from './components/Taskbar/Taskbar';
+import Window from './components/Window';
 
 function App() {
-  const [selectedCity, setSelectedCity] = useState<Search>();
-  const [units, setUnits] = useState('metric');
+  const [error, setError] = useState<string | null>(null);
+  const [wallpaperSet, setWallpaperSet] = useState(false);
+
+  useEffect(() => {
+    const getRandomWallpaper = async () => {
+      try {
+        const wallpaper = await fetchRandomWallpaper();
+        const divToInsertWallpaper = document.getElementById('root');
+
+        if (divToInsertWallpaper && !wallpaperSet) {
+          divToInsertWallpaper.style.backgroundImage = `url(${wallpaper.src})`;
+          setWallpaperSet(true);
+        }
+      } catch (error) {
+        setError("Random wallpaper couldn't be fetched");
+      }
+    };
+
+    if (!wallpaperSet) {
+      getRandomWallpaper();
+    }
+  }, [wallpaperSet]);
 
   return (
-    <div className='flex flex-col items-center min-h-screen py-2'>
-      <h1 className='text-4xl font-ms-bold font-bold mb-4'>Weather App</h1>
-      <SearchBar setSelectedCity={setSelectedCity} units={units} />
-      <WeatherDisplay
-        selectedCity={selectedCity}
-        units={units}
-        setUnits={setUnits}
-      />
+    <div className='w-full h-full flex justify-center items-center'>
+      <Window.Container>
+        <Window.Header />
+        <Window.Inner />
+      </Window.Container>
+
+      {error && <Prompt message={error} />}
+      <Taskbar />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '~/fonts/MSSansSerif.ttf';
 import '~/fonts/MSSansSerifBold.ttf';
 import App from './App.tsx';
 import './index.css';
+import { TaskbarProvider } from './providers/TaskbarProvider.tsx';
+import { WeatherSettingsProvider } from './providers/WeatherSettingsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <WeatherSettingsProvider>
+    <TaskbarProvider>
+      <App />
+    </TaskbarProvider>
+  </WeatherSettingsProvider>
 );
